c43b41d736802cf210f82b44e4f4456b96df1ac3
Jonathan Cortoppassi <106403752+cortoppassi@users.noreply.github.com>
2022-11-06 18:43:53 -0300

    Tela de Cadastro

---
+++ js/script.js
@@ -0,0 +1,71 @@
+const cadastros = []
+const cadastrados = []
+
+const telaDeCadastro = document.getElementById('telaDeCadastro');
+const telaDeLogin = document.getElementById('telaDeLogin')
+
+const nome = document.getElementById('cadastroNome');
+const email = document.getElementById('cadastroEmail');
+const senha = document.getElementById('cadastroSenha');
+const telefone = document.getElementById('cadastroTelefone');
+const data = document.getElementById('cadastroData');
+const masculino = document.getElementById('cadastroMasculino');
+const feminino = document.getElementById('cadastroFeminino');
+const estado = document.getElementById('cadastroEstado');
+
+const nomeLogin = document.getElementById('nome');
+const emailLogin = document.getElementById('email');
+const senhaLogin = document.getElementById('senha');
+
+function dadosDeCadastro() {
+    var determinarCadastro = {
+        nome: nome.value,
+        email: email.value,
+        senha: senha.value,
+        telefone: telefone.value,
+        data: data.value,
+        masculino: masculino.value,
+        feminino: feminino.value,
+        estado: estado.value
+    }
+    cadastros.push(determinarCadastro)
+    salvarNoLocalStorage()
+    abrirTelaDeLogin()
+};
+
+function logar() {
+    var logar = {
+        nome: nomeLogin.value,
+        email: emailLogin.value,
+        senha: senhaLogin.value
+       }
+
+    cadastros.map((item) => {
+        if  
+            (item.nome == logar.nome &&
+            item.email == logar.email &&
+            item.senha == logar.senha){
+                salvarNoLocalStorage()
+                alert('ok')
+        }else{
+            alert('Usuário não cadastrado.')
+        }
+    });
+};
+
+function salvarNoLocalStorage() {
+    localStorage.setItem('cadastros', JSON.stringify(cadastros));
+};
+
+function buscarNoLocalStorage() {
+    const storage = JSON.parse(localStorage.getItem(cadastros))
+};
+
+function abrirTelaDeCadastro() {
+    telaDeCadastro.style.display = 'block'
+    telaDeLogin.style.display = 'none'
+};
+function abrirTelaDeLogin() {
+    telaDeCadastro.style.display = 'none'
+    telaDeLogin.style.display = 'block'
+};
